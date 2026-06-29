2e84a41425d5565a9a3558e5dc8654b9cc7e31d0
Luan Rosa <luanrosa.ux@gmail.com>
2026-06-29 12:04:16 -0300

    added tag for google analytics

---
+++ js/analytics.js
@@ -0,0 +1,13 @@
+window.dataLayer = window.dataLayer || [];
+
+function gtag() {
+  dataLayer.push(arguments);
+}
+
+const googleTag = document.createElement("script");
+googleTag.async = true;
+googleTag.src = "https://www.googletagmanager.com/gtag/js?id=G-SYZ2W93FRE";
+document.head.appendChild(googleTag);
+
+gtag("js", new Date());
+gtag("config", "G-SYZ2W93FRE");
